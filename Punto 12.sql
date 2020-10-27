e3397782bbc2acb8312c4ce28778d49f4d395dc8
franko <francoastorga999@gmail.com>
2020-10-27 20:25:00 -0300

    Agrego total de idInsumo por pedido sin discriminar por modelo

--- Punto 12.sql
+++ Punto 12.sql
@@ -4,15 +4,17 @@ delimiter $$
 CREATE procedure listarCantidadInsumosPorPedido(in nro_pedido_input int)
 begin
 
-	select id_pedido, id_modelo, mxp.cantidad, m.nombre as modelo, lm.codigo as linea_montaje, e.nombre, ixe.id_insumo, ixe.cantidad_insumo, (ixe.cantidad_insumo * mxp.cantidad) as cant_insumos_total  from modelo_x_pedido mxp
+	select ixe.id_insumo, sum(ixe.cantidad_insumo) * sum(mxp.cantidad)  as cantidad_total from modelo_x_pedido mxp
 	inner join modelo m on mxp.id_pedido = m.codigo
 	inner join linea_de_montaje lm on m.codigo = lm.id_modelo_prod
 	inner join estaciones_x_linea exl on lm.codigo = exl.id_linea_de_montaje
 	inner join estacion e on e.codigo = exl.id_estacion
 	inner join insumo_x_estacion ixe on ixe.id_estacion = e.codigo
-	where mxp.id_pedido = nro_pedido_input;
+	where mxp.id_pedido = nro_pedido_input
+    group by ixe.id_insumo;
 
 END $$
+
 delimiter ;
 
 call listarCantidadInsumosPorPedido(1);
